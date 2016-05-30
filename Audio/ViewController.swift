014eb16696add2633027ed41a8bc31cc337758fa
ftwdjw <ftwdjw@gmail.com>
2016-05-30 16:11:45 -0600

    This iPhone program illustrates how to play an audio file using SWIFT.

--- Audio/ViewController.swift
+++ Audio/ViewController.swift
@@ -7,19 +7,71 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var PausePlay: UIButton!
+    
+    var ButtonAudioPlayer = try? AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("ButtonAudio", ofType: "wav")!))
+    
+    var BackgroundAudio = try? AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("BackgroundAudio", ofType: "mp3")!))
+
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
+        
+        BackgroundAudio!.play()
+        
+}
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func PlayAudio1(sender: AnyObject) {
+        ButtonAudioPlayer!.play()
+        
+    }
+    @IBAction func Stop(sender: AnyObject) {
+        BackgroundAudio!.stop()
+        BackgroundAudio!.currentTime = 0
+        
+        PausePlay.setTitle("Play", forState: UIControlState.Normal)
+        
+    }
 
+    @IBAction func Restart(sender: AnyObject) {
+        
+        BackgroundAudio!.stop()
+        BackgroundAudio!.currentTime = 0
+        BackgroundAudio!.play()
+        
+        
+    }
+    
+    
+    @IBAction func PausePlay(sender: AnyObject) {
+        
+        if (BackgroundAudio!.playing == true){
+            BackgroundAudio!.stop()
+            PausePlay.setTitle("Play", forState: UIControlState.Normal)
+            
+        }
+        else{
+            
+            BackgroundAudio!.play()
+            PausePlay.setTitle("Pause", forState: UIControlState.Normal)
+            
+            
+        }
+        
+        
+    }
+
+    
 }
 
